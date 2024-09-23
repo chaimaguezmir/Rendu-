8dd4f4efa9f721ca368d780a96c3dc12a462ef6b
ChaimaGz0 <chaima.guezmir20@gmail.com>
2024-09-23 20:15:04 +0100

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+
+import { FormsModule } from '@angular/forms';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { ListCategoriesComponent } from './components/list-categories/list-categories.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/user/user.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css'],
+})
+export class UserComponent {
+  titre: string = 'hello';
+  person = { id: 1, name: 'test', age: 20 };
+
+  quantity = 8;
+  image = '/assets/test.jpg';
+  height = 200;
+  width = 200;
+  color = 'blue';
+  acheter() {
+    this.quantity--;
+  }
+  onInputChange(event: Event) {
+    const el = event.target as HTMLInputElement;
+    this.color = el.value;
+  }
+}
